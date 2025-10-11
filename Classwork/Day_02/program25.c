029328cc049d2eab1e08dd12c8c3f2820435ca47
Aryandhumal27 <aryandhumal2710@gmail.com>
2025-10-11 13:25:50 +0530

    Add files via upload

---
+++ Classwork/Day_02/program25.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+//  Static function
+void Display()
+{
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt<=4; iCnt++)
+    {
+        prinf("Jay Ganesh...\n");
+    }
+}
+
+int main()
+{
+    Display();
+      
+    return 0;                             
+}
