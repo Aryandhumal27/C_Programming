3ef0fb9cc94595d89e7327625913a194838b2f94
Aryandhumal27 <aryandhumal2710@gmail.com>
2025-11-18 01:07:48 +0530

    Add files via upload

---
+++ Assignments/Assignment_24/program24_2.c
@@ -0,0 +1,58 @@
+// 2. Accept N numbers from user and return the smallest number.
+/*
+    Input :     N :         6
+                Elements :  85  66  3   77  93  88
+                
+    Output :    3   
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int Minimum(int Arr[], int iLength)
+{
+    int iCnt = 0, iMin = 0;
+
+    for(iCnt = 0, iMin = Arr[0]; iCnt < iLength; iCnt++)
+    {
+        if(Arr[iCnt] < iMin)
+        {
+            iMin = Arr[iCnt];
+        }
+    }
+
+    return iMin;
+}
+
+int main()
+{
+    int iCnt = 0, iSize = 0, iRet = 0;
+    int *p = NULL;
+
+    printf("Enter number of elements : ");
+    scanf("%d", &iSize);
+
+    p = (int *)malloc(iSize * sizeof(int));
+
+    if(NULL == p)
+    {
+        printf("Unable to allocate memory");
+        return -1;
+    }
+
+    printf("Enter %d elements\n", iSize);
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        printf("Enter element %d : ", (iCnt+1));
+        scanf("%d", &p[iCnt]);
+    }
+
+    iRet = Minimum(p, iSize);
+
+    printf("Smallest element is : %d", iRet);
+
+    free(p);
+
+    return 0;
+}
---
+++ Assignments/Assignment_24/program24_5.c
@@ -0,0 +1,63 @@
+// 5. Accept N numbers from user and display summation of digits of each number.
+/*
+    Input :     N :         6
+                Elements :  8225    665     3       76      953     858
+                
+    Output :    17  17  3   13  17  21
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void DigitsSum(int Arr[], int iLength)
+{
+    int iCnt = 0, iDigit = 0, iDigitSum = 0, iCurr = 0;
+
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        iDigitSum = 0;
+        iCurr = Arr[iCnt];
+
+        while(Arr[iCnt] > 0)
+        {
+            iDigit = Arr[iCnt] % 10;
+            iDigitSum = iDigitSum + iDigit;
+            Arr[iCnt] = Arr[iCnt] / 10;
+        }
+
+        Arr[iCnt] = iCurr;
+
+        printf("%d ", iDigitSum);
+    }
+}
+
+int main()
+{
+    int iCnt = 0, iSize = 0;
+    int *p = NULL;
+
+    printf("Enter the number of elements : ");
+    scanf("%d", &iSize);
+
+    p = (int *)malloc(iSize * sizeof(int));
+
+    if(NULL == p)
+    {
+        printf("Unable to allocate memory");
+        return -1;
+    }
+
+    printf("Enter %d elements : \n");
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        printf("Enter element %d : ", (iCnt + 1));
+        scanf("%d", &p[iCnt]);
+    }
+
+    DigitsSum(p, iSize);
+
+    free(p);
+
+    return 0;
+}
