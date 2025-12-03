a1618d523f9530c5268a086170692415f3fc6cd7
Aryandhumal27 <aryandhumal2710@gmail.com>
2025-12-04 01:21:53 +0530

    Add files via upload

---
+++ Assignments/Assignment_39/program39_1.c
@@ -0,0 +1,26 @@
+/*
+    1. Write a recursive program which display below pattern. 
+
+    Output :    *   *   *   *   *
+*/
+
+#include<stdio.h>
+
+void Display()
+{
+    static int iCnt = 1;
+
+    if(iCnt <= 5)
+    {
+        printf("*\t");
+        iCnt++;
+        Display();
+    }
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
---
+++ Assignments/Assignment_39/program39_4.c
@@ -0,0 +1,26 @@
+/*
+    4. Write a recursive program which display below pattern. 
+
+    Output :    A   B   C   D   E   F
+*/
+
+#include<stdio.h>
+
+void Display()
+{
+    static char ch = 'A';
+
+    if(ch <= ('A' + 5))
+    {
+        printf(" %c\t", ch);
+        ch++;
+        Display();
+    }
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
