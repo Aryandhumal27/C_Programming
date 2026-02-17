0f8246846ce1a7a14ffd7d6a50b08e891348bea7
Aryandhumal27 <aryandhumal2710@gmail.com>
2026-02-17 15:24:38 +0530

    Add files via upload

---
+++ Assignments/Assignment_69/program69_1.c
@@ -0,0 +1,31 @@
+/*
+    1. Write a recursive program which display below pattern
+
+    Input : 5
+    Output :    *   *   *   *   *
+*/
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    static int i = 1;
+
+    if(i <= iNo)
+    {
+        printf("*\t");
+        i++;
+        Display(iNo);
+    }
+}
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the number\n");
+    scanf("%d", &iValue);
+
+    Display(iValue);
+
+    return 0;
+}
